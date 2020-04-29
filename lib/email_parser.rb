2469e50e88dcf4d8b745727b640cf943e62edfb2
Ashley Rodriguez <arodriguez1030@gmail.com>
2020-04-29 19:25:55 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailAddressParser
     parse
   end
   
-  def self.parse
+  def parse
     email_array = @email.split(/[,]/).uniq
     email_array.reject! {|element| element.empty?}
     email_array
